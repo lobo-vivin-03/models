1a63618385e3fb56ccd2ce0a6d983fd22b835b70
lobo-vivin-03 <vivinlobo5@gmail.com>
2024-11-25 00:15:05 +0530

    commit

--- yt/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-if 'and' in subject_name:
-    #     subject_name = subject_name.split('and')[0].strip()
-    # elif '&' in subject_name:
-    #     subject_name = subject_name.split('&')[0].strip()
